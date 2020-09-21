0b1a5887bb27b4f1f5c9c1798b62e5bebc5097a8
leigharobinson <lmarobinson24@gmail.com>
2020-09-21 16:19:04 -0500

    got modal detial for Song Response to work

--- src/components/songresponse/SongResponseCard.js
+++ src/components/songresponse/SongResponseCard.js
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SongResponse.css";
-// import { SongResponseDetail } from "../songresponse/SongResponseDetails";
+import { SongResponseDetail } from "../songresponse/SongResponseDetails";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import "./SongResponse.css";
 
 export const SongResponseCard = (props) => {
+  const [show, setShow] = useState(false);
+
   const dateCreated = props.songResponse.created_at;
   const songTitle = props.song;
   const artist = props.artist;
-  // const eye_contact = props.eye_contact;
-  // const talkativeness = props.talkativeness;
-  // const mood = props.mood;
-  // const movement = props.movement;
-  // const vocalization = props.vocalization;
-  // const liked_song = props.liked_song;
-  // const notes = props.songResponse.notes;
+  const eye_contact = props.eye_contact;
+  const talkativeness = props.talkativeness;
+  const mood = props.mood;
+  const movement = props.movement;
+  const vocalization = props.vocalization;
+  const liked_song = props.liked_song;
+  const notes = props.songResponse.notes;
 
   const eye_contact_score = parseInt(props.eye_contact_id);
   const talkativeness_score = parseInt(props.talkativeness_id);
@@ -20,6 +25,9 @@ export const SongResponseCard = (props) => {
   const movement_score = parseInt(props.movement_id);
   const vocalization_score = parseInt(props.vocalization_id);
   const liked_song_score = parseInt(props.liked_song_id);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   const totalScore =
     eye_contact_score +
@@ -40,13 +48,47 @@ export const SongResponseCard = (props) => {
         <h5>
           <strong>"{songTitle}"</strong> by: {artist}
         </h5>
-      </div>
-      <div>
-        {/* <SongResponseDetail
-          {...props}
-          dateCreated={dateCreated}
-          songTitle={songTitle}
-        /> */}
+        <div>
+          <Button variant="primary" onClick={handleShow}>
+            Details
+          </Button>
+
+          <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>
+                <h1>Song Response Details</h1>
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <SongResponseDetail
+                {...props}
+                totalScore={totalScore}
+                dateCreated={dateCreated}
+                songTitle={songTitle}
+                eye_contact={eye_contact}
+                eye_contact_score={eye_contact_score}
+                talkativeness={talkativeness}
+                talkativeness_score={talkativeness_score}
+                mood={mood}
+                mood_score={mood_score}
+                movement={movement}
+                movement_score={movement_score}
+                vocalization={vocalization}
+                vocalization_score={vocalization_score}
+                liked_song={liked_song}
+                liked_song_score={liked_song_score}
+              />
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Close
+              </Button>
+              <Button variant="primary" onClick={handleClose}>
+                Save Changes
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        </div>
       </div>
     </>
   );
--- src/components/songresponse/SongResponseDetails.js
+++ src/components/songresponse/SongResponseDetails.js
@@ -1,14 +1,48 @@
 import React from "react";
-// import "./SongResponse.css";
+import "./SongResponse.css";
 
 export const SongResponseDetail = (props) => {
-  //   const dateCreated = props.songResponse.created_at;
-  //   const song = props.song;
-  //   const artist = props.artist;
-
   return (
     <>
-      <h1> {props.dateCreated}</h1>
+      <div>
+        <h4> {props.dateCreated}</h4>
+        <h5>
+          <strong>"{props.songTitle}"</strong> by: {props.artist}
+          <hr />
+          <div className="Parent">
+            <p>Eye Contact: {props.eye_contact}</p>
+            <p>{props.eye_contact_score}/5 </p>
+          </div>
+          <div className="Parent">
+            <p>Talkativeness: {props.talkativeness}</p>
+            <p> {props.talkativeness_score}/5</p>
+          </div>
+          <div className="Parent">
+            <p>Mood: {props.mood}</p>
+            <p>{props.mood_score}/5</p>
+          </div>
+          <div className="Parent">
+            <p>Movement: {props.movement}</p>
+            <p>{props.movement_score}/5</p>
+          </div>
+          <div className="Parent">
+            <p>Vocalization: {props.vocalization}</p>
+            <p>{props.vocalization_score}/5</p>
+          </div>
+          <div className="Parent">
+            <p>Liked Song: {props.liked_song}</p>
+            <p>{props.liked_song_score}/5</p>
+          </div>
+          <div className="Parent">
+            <h4>
+              <strong> Score:</strong>
+            </h4>
+            <h4>
+              <strong>{props.totalScore}/36</strong>
+            </h4>
+          </div>
+        </h5>
+      </div>
     </>
   );
 };
